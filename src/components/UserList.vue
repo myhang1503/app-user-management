1bf297aac24dc7ec2eeb4a0c5717c74e65492170
ptmhang <myhang1503@gmail.com>
2023-07-12 18:49:39 +0700

    state

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,19 +1,39 @@
 <template>
   <tbody>
-    <user-item></user-item>
-    <user-item></user-item>
-    <user-item></user-item>
-    <user-item></user-item>
-    <user-item></user-item>
+    <user-item
+      v-for="(user, index) in userList"
+      :key="index"
+      :user="user"
+    ></user-item>
   </tbody>
 </template>
 
 <script>
 import UserItem from "./UserItem.vue";
+import { mapState } from "vuex";
+
 export default {
   components: {
     UserItem,
   },
+  //State: use $store cua vue
+  // computed: {
+  //   userList(){
+  //     return this.$store.state.userList;
+  //   },
+  // },
+
+  //mapState
+  computed: {
+    loading() {
+      return false;
+    },
+    ...mapState({
+      userList: (state) => state.userList,
+      //userList: "userList", //cach viet khac
+    }),
+  },
+  //computed: mapState(["userList"]), //cach viet khac
 };
 </script>
 
